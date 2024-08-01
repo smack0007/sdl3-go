9026b9e68809b8bf75f205633ec69daef963669b
Zachary Snow <zachary.snow@gmail.com>
2024-08-01 16:24:03 +0200

    Add SDL Logging functions.

--- examples/hello-world/main.go
+++ examples/hello-world/main.go
@@ -1,127 +1,128 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/smack0007/sdl_go/sdl"
+	SDL "github.com/smack0007/sdl_go/sdl"
 )
 
 func main() {
-	if sdl.Init(sdl.INIT_VIDEO) != 0 {
-		fmt.Println("Failed initialize SDL.")
+	if SDL.Init(SDL.INIT_VIDEO) != 0 {
+		SDL.LogError(SDL.LOG_CATEGORY_APPLICATION, "Failed initialize SDL.")
 		os.Exit(1)
 	}
-	defer sdl.Quit()
+	defer SDL.Quit()
 
-	var window *sdl.Window
-	var renderer *sdl.Renderer
-	result := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN, &window, &renderer)
+	SDL.LogSetPriority(SDL.LOG_CATEGORY_APPLICATION, SDL.LOG_PRIORITY_DEBUG);
+
+	var window *SDL.Window
+	var renderer *SDL.Renderer
+	result := SDL.CreateWindowAndRenderer(800, 600, SDL.WINDOW_SHOWN, &window, &renderer)
 
 	if result != 0 {
-		fmt.Println("Failed to create window and renderer.")
+		SDL.LogError(SDL.LOG_CATEGORY_APPLICATION, "Failed to create window and renderer.")
 		os.Exit(1)
 	}
 
-	defer sdl.DestroyWindow(window)
-	defer sdl.DestroyRenderer(renderer)
+	defer SDL.DestroyWindow(window)
+	defer SDL.DestroyRenderer(renderer)
 
-	sdl.SetWindowTitle(window, "Hello World!")
+	SDL.SetWindowTitle(window, "Hello World!")
 
-	fmt.Println("Initialized.")
+	SDL.LogInfo(SDL.LOG_CATEGORY_APPLICATION, "Initialized.")
 
-	event := sdl.Event{}
+	event := SDL.Event{}
 	done := false
 	for !done {
-		for sdl.PollEvent(&event) > 0 {
+		for SDL.PollEvent(&event) > 0 {
 			switch event.Type() {
 
-			case sdl.QUIT:
+			case SDL.QUIT:
 				done = true
 
-			case sdl.WINDOWEVENT:
+			case SDL.WINDOWEVENT:
 				switch event.Window.Event() {
-				case sdl.WINDOWEVENT_ENTER:
-					fmt.Println("Enter")
+				case SDL.WINDOWEVENT_ENTER:
+					SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Enter")
 
-				case sdl.WINDOWEVENT_LEAVE:
-					fmt.Println("Leave")
+				case SDL.WINDOWEVENT_LEAVE:
+					SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Leave")
 				}
 
-			case sdl.KEYDOWN:
-				fmt.Println("Key Down", event.Key.Keysym())
+			case SDL.KEYDOWN:
+				SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Key Down %d", event.Key.Keysym())
 
-			case sdl.KEYUP:
-				fmt.Println("Key Up", event.Key.Keysym())
+			case SDL.KEYUP:
+				SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Key Up %d", event.Key.Keysym())
 
-			case sdl.MOUSEMOTION:
-				fmt.Println("Mouse Motion", event.Motion.X(), event.Motion.Y())
+			case SDL.MOUSEMOTION:
+				SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Mouse Motion (%d, %d)", event.Motion.X(), event.Motion.Y())
 
-			case sdl.MOUSEBUTTONDOWN:
-				fmt.Println("Mouse Button Down", event.Button.Button())
+			case SDL.MOUSEBUTTONDOWN:
+				SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Mouse Button Down %d", event.Button.Button())
 
-			case sdl.MOUSEBUTTONUP:
-				fmt.Println("Mouse Button Up", event.Button.Button())
+			case SDL.MOUSEBUTTONUP:
+				SDL.LogDebug(SDL.LOG_CATEGORY_APPLICATION, "Mouse Button Up %d", event.Button.Button())
 			}
 		}
 
 		draw(renderer)
-		sdl.Delay(1)
+		SDL.Delay(1)
 	}
 
-	fmt.Println("Shuting down...")
+	SDL.LogInfo(SDL.LOG_CATEGORY_APPLICATION, "Shuting down...")
 }
 
-func draw(renderer *sdl.Renderer) {
-	sdl.SetRenderDrawColor(renderer, 100, 149, 237, 255)
-	sdl.RenderClear(renderer)
+func draw(renderer *SDL.Renderer) {
+	SDL.SetRenderDrawColor(renderer, 100, 149, 237, 255)
+	SDL.RenderClear(renderer)
 
-	sdl.SetRenderDrawColor(renderer, 255, 0, 0, 255)
+	SDL.SetRenderDrawColor(renderer, 255, 0, 0, 255)
 
-	points := []sdl.Point{
-		sdl.Point{X: 0, Y: 0},
-		sdl.Point{X: 1, Y: 0},
-		sdl.Point{X: 2, Y: 0},
-		sdl.Point{X: 3, Y: 0},
-		sdl.Point{X: 4, Y: 0},
-		sdl.Point{X: 5, Y: 0},
+	points := []SDL.Point{
+		SDL.Point{X: 0, Y: 0},
+		SDL.Point{X: 1, Y: 0},
+		SDL.Point{X: 2, Y: 0},
+		SDL.Point{X: 3, Y: 0},
+		SDL.Point{X: 4, Y: 0},
+		SDL.Point{X: 5, Y: 0},
 
-		sdl.Point{X: 0, Y: 1},
-		sdl.Point{X: 1, Y: 1},
-		sdl.Point{X: 2, Y: 1},
-		sdl.Point{X: 3, Y: 1},
-		sdl.Point{X: 4, Y: 1},
-		sdl.Point{X: 5, Y: 1},
+		SDL.Point{X: 0, Y: 1},
+		SDL.Point{X: 1, Y: 1},
+		SDL.Point{X: 2, Y: 1},
+		SDL.Point{X: 3, Y: 1},
+		SDL.Point{X: 4, Y: 1},
+		SDL.Point{X: 5, Y: 1},
 
-		sdl.Point{X: 0, Y: 2},
-		sdl.Point{X: 1, Y: 2},
-		sdl.Point{X: 2, Y: 2},
-		sdl.Point{X: 3, Y: 2},
-		sdl.Point{X: 4, Y: 2},
-		sdl.Point{X: 5, Y: 2},
+		SDL.Point{X: 0, Y: 2},
+		SDL.Point{X: 1, Y: 2},
+		SDL.Point{X: 2, Y: 2},
+		SDL.Point{X: 3, Y: 2},
+		SDL.Point{X: 4, Y: 2},
+		SDL.Point{X: 5, Y: 2},
 
-		sdl.Point{X: 0, Y: 3},
-		sdl.Point{X: 1, Y: 3},
-		sdl.Point{X: 2, Y: 3},
-		sdl.Point{X: 3, Y: 3},
-		sdl.Point{X: 4, Y: 3},
-		sdl.Point{X: 5, Y: 3},
+		SDL.Point{X: 0, Y: 3},
+		SDL.Point{X: 1, Y: 3},
+		SDL.Point{X: 2, Y: 3},
+		SDL.Point{X: 3, Y: 3},
+		SDL.Point{X: 4, Y: 3},
+		SDL.Point{X: 5, Y: 3},
 
-		sdl.Point{X: 0, Y: 4},
-		sdl.Point{X: 1, Y: 4},
-		sdl.Point{X: 2, Y: 4},
-		sdl.Point{X: 3, Y: 4},
-		sdl.Point{X: 4, Y: 4},
-		sdl.Point{X: 5, Y: 4},
+		SDL.Point{X: 0, Y: 4},
+		SDL.Point{X: 1, Y: 4},
+		SDL.Point{X: 2, Y: 4},
+		SDL.Point{X: 3, Y: 4},
+		SDL.Point{X: 4, Y: 4},
+		SDL.Point{X: 5, Y: 4},
 
-		sdl.Point{X: 0, Y: 5},
-		sdl.Point{X: 1, Y: 5},
-		sdl.Point{X: 2, Y: 5},
-		sdl.Point{X: 3, Y: 5},
-		sdl.Point{X: 4, Y: 5},
-		sdl.Point{X: 5, Y: 5},
+		SDL.Point{X: 0, Y: 5},
+		SDL.Point{X: 1, Y: 5},
+		SDL.Point{X: 2, Y: 5},
+		SDL.Point{X: 3, Y: 5},
+		SDL.Point{X: 4, Y: 5},
+		SDL.Point{X: 5, Y: 5},
 	}
-	sdl.RenderDrawPoints(renderer, points, 36)
+	SDL.RenderDrawPoints(renderer, points, 36)
 
-	sdl.RenderPresent(renderer)
+	SDL.RenderPresent(renderer)
 }
