b981054d80891fadacf1119dc7c2b65c8ae1a9c5
smack0007 <zachary.snow@gmail.com>
2025-03-02 17:23:22 +0100

    Update docker tag

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,4 +3,4 @@ set -e
 SCRIPT_DIRECTORY="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 
 cd ${SCRIPT_DIRECTORY}/..
-docker build -t sdl-go .
+docker build -t smack0007/sdl-go .
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -3,4 +3,4 @@ set -e
 SCRIPT_DIRECTORY="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 
 cd ${SCRIPT_DIRECTORY}/..
-docker run -it --rm -v .:/app sdl-go /bin/sh
+docker run -it --rm -v .:/app smack0007/sdl-go /bin/sh
