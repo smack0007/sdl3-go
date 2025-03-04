910af72c735cd99327959c674554cdbcc4051c0d
smack0007 <zachary.snow@gmail.com>
2025-03-04 14:40:35 +0100

    docker-run.sh no longer uses -it flag in CI.

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -2,5 +2,10 @@
 set -e
 . "$(dirname $(realpath "${BASH_SOURCE[0]}"))/../env.sh"
 
+DOCKER_FLAGS="--rm -v .:/app"
+if [[ ! "${CI}" = "1" ]]; then 
+  DOCKER_FLAGS="${DOCKER_FLAGS} -it"
+fi
+
 cd ${REPO_PATH}
-docker run -it --rm -v .:/app smack0007/sdl-go /bin/sh
+docker run ${DOCKER_FLAGS} smack0007/sdl-go /bin/sh
